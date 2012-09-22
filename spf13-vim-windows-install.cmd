b99ab5104bf9178c2d4af03c20a433f0073fc6a1
stonelee <istonelee@gmail.com>
2012-09-22 16:43:25 +0800

    windows下使用

--- spf13-vim-windows-install.cmd
+++ spf13-vim-windows-install.cmd
@@ -2,10 +2,13 @@
 @if not exist "%HOME%" @set HOME=%USERPROFILE%
 
 @set BASE_DIR=%HOME%\.spf13-vim-3
-call git clone --recursive -b 3.0 git://github.com/spf13/spf13-vim.git %BASE_DIR%
+call git clone --recursive -b 3.0 git://github.com/stonelee/spf13-vim.git %BASE_DIR%
 call mkdir %BASE_DIR%\.vim\bundle
 call mklink /J %HOME%\.vim %BASE_DIR%\.vim
 call mklink %HOME%\.vimrc %BASE_DIR%\.vimrc
-call mklink %HOME%\_vimrc %BASE_DIR%\.vimrc
+" call mklink %HOME%\_vimrc %BASE_DIR%\.vimrc
+
+call mklink %HOME%\.vimrc.local %BASE_DIR%\.vimrc.local
+call mklink %HOME%\.vimrc.bundles.local %BASE_DIR%\.vimrc.bundles.local
 
 call git clone http://github.com/gmarik/vundle.git %HOME%/.vim/bundle/vundle
